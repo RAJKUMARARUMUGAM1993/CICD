960d6d1cb608debef24cfbd8c49536fc53dc6da6
Rajkumar <rajkumaram3579@gmail.com>
2025-07-29 13:23:29 +0530

    work

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/get', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/update', (req, res) => {
+  res.send('CICD Pipeline working...!');
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
